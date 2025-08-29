50b58d3dd6b615891e59dd11a98318f9a948bc24
naeunmin <babaleo115@gmail.com>
2025-08-29 09:40:41 +0900

    init

---
+++ src/main/java/com/api/tokbaro/domain/user/entity/User.java
@@ -0,0 +1,4 @@
+package com.api.tokbaro.domain.user.entity;
+
+public class User {
+}
---
+++ src/test/java/com/api/tokbaro/TokbaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.tokbaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokbaroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
