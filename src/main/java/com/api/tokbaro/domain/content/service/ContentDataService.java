fa26bea0a4add76b50900109dd80aa2fb8f27a2c
naeunmin <babaleo115@gmail.com>
2025-09-07 13:08:48 +0900

    feat : 반응속도 top10 가져오기

--- src/main/java/com/api/tokbaro/domain/content/service/ContentDataService.java
+++ src/main/java/com/api/tokbaro/domain/content/service/ContentDataService.java
@@ -2,8 +2,13 @@ package com.api.tokbaro.domain.content.service;
 
 
 import com.api.tokbaro.domain.content.web.dto.ReactionReq;
+import com.api.tokbaro.domain.content.web.dto.ReactionVelocityRes;
 import com.api.tokbaro.global.jwt.UserPrincipal;
+
+import java.util.List;
 
 public interface ContentDataService {
     void saveReactionVelocity(Long userId, ReactionReq reactionReq);
+
+    List<ReactionVelocityRes> getRanking();
 }
