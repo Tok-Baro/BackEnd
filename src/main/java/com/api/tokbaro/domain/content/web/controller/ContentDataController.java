1e1828ddc3db04830a4a4277c7f21931a881fec4
naeunmin <babaleo115@gmail.com>
2025-09-09 21:26:26 +0900

    refactor : 엔드포인트수정

--- src/main/java/com/api/tokbaro/domain/content/web/controller/ContentDataController.java
+++ src/main/java/com/api/tokbaro/domain/content/web/controller/ContentDataController.java
@@ -4,6 +4,7 @@ import com.api.tokbaro.domain.content.service.ContentDataService;
 import com.api.tokbaro.domain.content.service.ContentDataServiceImpl;
 import com.api.tokbaro.domain.content.web.dto.ReactionReq;
 import com.api.tokbaro.domain.content.web.dto.ReactionVelocityRes;
+import com.api.tokbaro.domain.user.entity.User;
 import com.api.tokbaro.global.jwt.UserPrincipal;
 import com.api.tokbaro.global.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
@@ -23,18 +24,18 @@ public class ContentDataController {
 
     //이거 나중에 JWT방식으로 갈아야함 통신 테스트하려고
     //반응속도 저장
-    @PostMapping("/users/me/reactions/{userId}")
+    @PostMapping("/users/me/reactions")
     public ResponseEntity<SuccessResponse<?>> saveReactionVelocity(
-            @PathVariable Long userId,
+            @AuthenticationPrincipal UserPrincipal userPrincipal,
             @RequestBody ReactionReq reactionReq){
-        contentDataService.saveReactionVelocity(userId, reactionReq);
+        contentDataService.saveReactionVelocity(userPrincipal.getId(), reactionReq);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(SuccessResponse.success("데이터 저장에 성공했습니다."));
     }
 
     //반응속도 랭킹 반환
-    @GetMapping("/users/reactions")
+    @GetMapping("/users/me/reactions")
     public ResponseEntity<SuccessResponse<?>> getRanking(){
         List<ReactionVelocityRes> rankingList = contentDataService.getRanking();
         return ResponseEntity
