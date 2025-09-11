3343e6af414852eb76a8fd8e4cd00bcf845b9919
naeunmin <babaleo115@gmail.com>
2025-09-11 12:34:20 +0900

    feat : email 추가

--- src/main/java/com/api/tokbaro/domain/user/web/dto/AppleIdReq.java
+++ src/main/java/com/api/tokbaro/domain/user/web/dto/AppleIdReq.java
@@ -7,4 +7,5 @@ public class AppleIdReq {
     private String identityToken; //사용자 토큰, 애플이 발급한 JWT
     private String givenName; //이름
     private String familyName; //이름 : 성
+    private String email;
 }
