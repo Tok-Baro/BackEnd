f9ed68c973f92b724bec835f39ff9a834decf70b
naeunmin <babaleo115@gmail.com>
2025-09-18 10:44:30 +0900

    feat : 유저데이터 예외처리코드 추가

--- src/main/java/com/api/tokbaro/global/response/code/user/UserErrorResponseCode.java
+++ src/main/java/com/api/tokbaro/global/response/code/user/UserErrorResponseCode.java
@@ -13,7 +13,8 @@ public enum UserErrorResponseCode implements BaseResponseCode {
     USER_UNAUTHORIZED_401("USER_UNAUTHORIZED_401", 401, "비밀번호가 일치하지 않습니다."),
     INVALID_APPLE_ID_TOKEN_401("INVALID_APPLE_ID_TOKEN_401", 401, "토큰이 유효하지 않음"),
     INVAILD_REFRESH_TOKEN_401("INVAILD_REFRESH_TOKEN_401", 401, "리프레쉬 토큰이 유효하지 않습니다."),
-    USER_NOT_FOUND_FOR_TOKEN_404("USER_NOT_FOUND_FOR_TOKEN_404", 404, "토큰에 해당하는 사용자를 찾을 수 없습니다.");
+    USER_NOT_FOUND_FOR_TOKEN_404("USER_NOT_FOUND_FOR_TOKEN_404", 404, "토큰에 해당하는 사용자를 찾을 수 없습니다."),
+    CONTENT_DATA_NOT_FOUND_404("CONTENT_DATA_NOT_FOUND_404", 404, "유저의 콘텐츠데이터가 존재하지 않습니다.");
     private final String code;
     private final int httpStatus;
     private final String message;
