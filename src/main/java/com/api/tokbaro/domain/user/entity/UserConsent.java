6c73dd140d67be421ee7ee66be82b5f5b50c74f3
naeunmin <babaleo115@gmail.com>
2026-01-02 19:09:51 +0900

    feat : 이용약관 동의 각 항목의 동의 여부 처리 각 항목에 대해 동의여부를 엔티티로 관리

---
+++ src/main/java/com/api/tokbaro/domain/user/entity/UserConsent.java
@@ -0,0 +1,45 @@
+package com.api.tokbaro.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "user_consents")
+public class UserConsent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "consent_type", nullable = false)
+    private ConsentType consentType;
+
+    @Column(name = "is_agreed", nullable = false)
+    private boolean isAgreed;
+
+    @Column(name = "agreed_at")
+    private LocalDateTime agreedAt;
+
+    @Column(name = "withdrawn_at")
+    private LocalDateTime withdrawnAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder
+    public UserConsent(User user, ConsentType consentType, boolean isAgreed, LocalDateTime agreedAt, LocalDateTime withdrawnAt) {
+        this.user = user;
+        this.consentType = consentType;
+        this.isAgreed = isAgreed;
+        this.agreedAt = agreedAt;
+        this.withdrawnAt = withdrawnAt;
+    }
+}
