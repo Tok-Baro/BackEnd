c63332c9947698ca945397c169062b68f062b4bb
naeunmin <babaleo115@gmail.com>
2026-01-03 17:39:41 +0900

    feat : 기본 생성자를 protected로 설정

--- src/main/java/com/api/tokbaro/domain/user/entity/User.java
+++ src/main/java/com/api/tokbaro/domain/user/entity/User.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Entity
 @Getter
 @Builder
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Table(name = "users")
 public class User extends BaseEntity {
