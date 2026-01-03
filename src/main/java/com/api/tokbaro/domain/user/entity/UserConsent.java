8268ec0c16cf4ee2699be410eba23b4a29e31e3e
naeunmin <babaleo115@gmail.com>
2026-01-03 17:19:59 +0900

    feat : 기본 생성자 protected로 설정

--- src/main/java/com/api/tokbaro/domain/user/entity/UserConsent.java
+++ src/main/java/com/api/tokbaro/domain/user/entity/UserConsent.java
@@ -1,6 +1,7 @@
 package com.api.tokbaro.domain.user.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "user_consents")
 public class UserConsent {
 
