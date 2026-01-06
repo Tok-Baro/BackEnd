25fa511ea282af3127d0b4b3a0cd067476fb0e47
naeunmin <babaleo115@gmail.com>
2026-01-06 18:15:22 +0900

    refactor : username 삭제로 쿼리 변경

--- src/main/java/com/api/tokbaro/domain/user/repository/UserRepository.java
+++ src/main/java/com/api/tokbaro/domain/user/repository/UserRepository.java
@@ -6,12 +6,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
+    Optional<User> findByNickname(String nickname);
 
-    Boolean existsByUsername(String username);
+    Boolean existsByNickname(String nickname);
 
-    Optional<User> findByAppleId(String appleId);
-
+    Optional<User> findByEmail(String email);
 
     Boolean existsByEmail(String email);
+
+    Optional<User> findByProviderId(String providerId);
+
+
+
 }
