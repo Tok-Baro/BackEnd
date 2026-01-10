fa76bd16cbfeb86908f9f00b56d1c577c1fef521
naeunmin <babaleo115@gmail.com>
2026-01-10 14:09:38 +0900

    feat: 회원가입 시 Request에 동의항목추가

--- src/main/java/com/api/tokbaro/domain/user/web/dto/SignUpUserReq.java
+++ src/main/java/com/api/tokbaro/domain/user/web/dto/SignUpUserReq.java
@@ -2,9 +2,12 @@ package com.api.tokbaro.domain.user.web.dto;
 
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class SignUpUserReq {
     private String email;
     private String nickname;
     private String password;
+    private List<ConsentDto> consents; //동의항목리스트
 }
